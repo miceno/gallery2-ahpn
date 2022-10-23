7c5d0b63ebcca1a63213cbe3f359556694c3c043
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2022-10-23 17:15:27 +0200

    Remove sensitive data from file.

--- ga.js
+++ ga.js
@@ -1,20 +1,20 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11726448-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=XXXXXX"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-11726448-1');
+  gtag('config', 'UA-XXXX-1');
 </script>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DF5Z8HLWYM"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=ZZZZZZZZZ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-DF5Z8HLWYM');
+  gtag('config', 'G-ZZZZZZZ');
 </script>
 
